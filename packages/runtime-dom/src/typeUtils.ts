7121c486415a4e5370897bb0c7f1d4e83ae4dc9a
allen <>
2025-01-03 00:24:25 +0800

    add comment

--- packages/runtime-dom/src/typeUtils.ts
+++ packages/runtime-dom/src/typeUtils.ts
@@ -4,4 +4,14 @@ export type UnionToIntersection<U> = (
   ? I
   : never;
 
+/**
+ * 檢測型別是否為 `any`。
+ * If `T` is `any`, returns `Y`, otherwise returns `N`.
+ *
+ * @template T - The type to check.
+ * @template Y - The type to return if `T` is `any`.
+ * @template N - The type to return if `T` is not `any`.
+ */
 export type IfAny<T, Y, N> = 0 extends 1 & T ? Y : N;
+
+// let data: IfAny<"1", string, number> = "1";
